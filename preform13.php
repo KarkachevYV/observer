2d958a3b7440c3a9d6a9f90bca6c7482e7fe6367
Каркач_Ю_В <pchelovod2902@gmail.com>
2020-06-12 23:22:13 +0300

    добовляю народное вече

---
+++ preform13.php
@@ -0,0 +1,230 @@
+<?
+$title = "Народное вече";
+require_once("components/header.php");
+?>
+<style>
+  .colortext {
+    font-family: 'Lato', sans-serif;
+    font-size: 1.5vw;
+    color: black;      
+}
+  li {
+    font-size: 0.4vw;
+  }
+  table, td, th {
+    font-size: 1.0vw;
+	  border: 1px solid black;
+  }
+  h6 {
+    font-size: 0.7vw;
+  }
+  h8 {
+    font-size: 0.4vw;
+  }
+</style>
+<div class="container">
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="preform.php" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="preform2.php">2</a></li>
+      <li class="page-item"><a class="page-link" href="preform3.php">3</a></li>
+      <li class="page-item"><a class="page-link" href="preform4.php">4</a></li>
+      <li class="page-item"><a class="page-link" href="preform5.php">5</a></li>
+      <li class="page-item"><a class="page-link" href="preform6.php">6</a></li>
+      <li class="page-item"><a class="page-link" href="preform7.php">7</a></li>
+      <li class="page-item"><a class="page-link" href="preform8.php">8</a></li>
+      <li class="page-item"><a class="page-link" href="preform9.php">9</a></li>
+      <li class="page-item"><a class="page-link" href="preform10.php">10</a></li>
+      <li class="page-item"><a class="page-link" href="preform11.php">11</a></li>
+      <li class="page-item"><a class="page-link" href="preform12.php">12</a></li>
+      <li class="page-item"><a class="page-link" href="preform13.php">13</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <div class="row">
+    <div class="col-12">
+      <i>
+      <div class="row">
+        <div class="col-12">
+          <table>
+            <caption><b>Приложение 4</b> - Форма № 4 технологии &laquo;Народное Вече&raquo;</caption>
+            <tr>
+              <td><img src="resource/img/4.jpg"  width="75%"  alt=""></th>
+              <td>
+                <p>Союз избирателей России &laquo;Народное  Вече&raquo;.</p>
+                <h4><b>Протокол обобщения первого уровня непосредственного волеизъявления Граждан (избирателей) РФ до Воли соответствующей части Народа России </b></h4>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <p><b>ФОРМУЛА ВОЛЕИЗЪЯВЛЕНИЯ – На основании своего конституционного права непосредственно участвовать в управлении делами государства (ст.32, п.1 действующей Конституции РФ) я, Гражданин Российской Федерации, находясь в здравом уме и светлой памяти, добровольно и осознанно присоединяю свой голос к непосредственному волеизъявлению части Народа РФ по формуле: &laquo;Запрещаю органам государственной власти РФ изменять сроки наступления (пенсионный возраст) гарантированно-го ст.39, п.1 действующей Конституции РФ права каждого на социальное обеспе-чение (пенсию)  по возрасту относительно их состояния на 01 января 2015 года.&raquo;</b></p>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <table width="100%">
+                  <tr>
+                    <th rowspan="2" width="5%">№ п/п</th>
+                    <th rowspan="2" width="40%">Фамилия, имя, отчество (полностью) члена Исполкома первого уровня - представителя по доверенности первичной ячейки Союза избирателей России &laquo;Народное Вече&raquo;</th>
+                    <th rowspan="2" width="10%">Всего избирателей в первичной ячейке</th>
+                    <th colspan="2" width="20%"></th>
+                    <th rowspan="2" width="25%"><h8>&laquo;За достоверность представленных данных отвечаю&raquo;</h8> Подпись и дата</th>
+                    <td>&laquo;ДА&raquo;</td><td>&laquo;НЕТ&raquo;</td>
+                  </tr>
+                  <tr>
+                    <td>1.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>2.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>3.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>4.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>5.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>6.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>       
+                  <tr>
+                    <td>7.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>8.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>9.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>10.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>11.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>12.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>13.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>14.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>15.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td colspan="2">Обобщенный итог волеизъявления</td>                  
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td colspan="6">
+                      <h6>Обобщение итогов волеизъявления ... избирателей проведено Исполкомом первого уровня Союза избирателей России  &laquo;Народное Вече&raquo; № ...</h6>
+                      <h6>&laquo; ... &raquo;...  2020 г.</h6>
+                      <h6>Секретарь Исполкома      подпись      дата       расшифровка фамилии, имени, отчества</h6>
+                      <h6>Председатель   Исполкома       подпись      дата       расшифровка фамилии, имени, отчества</h6>
+                      <h6>Представитель в Исполкоме 2 уровня       подпись      дата       расшифровка фамилии, имени, отчества</h6>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      </i>
+    </div>
+  </div>  
+</div>
+
+<?
+require_once("components/footer.php");
+?>
