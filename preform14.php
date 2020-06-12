6921d02ada0cbbb4f945c9f5bf84811a655ff339
Yriu2902VK <pchelovod2902@gmail.com>
2020-06-13 02:31:42 +0300

    народное вече готово

---
+++ preform14.php
@@ -0,0 +1,91 @@
+<?
+$title = "Народное вече";
+require_once("components/header.php");
+?>
+<style>
+  .colortext {
+    font-family: 'Lato', sans-serif;
+    font-size: 1.5vw;
+    color: black;      
+}
+  li {
+    font-size: 0.4vw;
+  }
+  table, td, th {
+    font-size: 1.0vw;
+	  border: 1px solid black;
+  }
+  h6 {
+    font-size: 0.7vw;
+  }
+  h8 {
+    font-size: 0.4vw;
+  }
+</style>
+<div class="container">
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="preform.php" aria-label="Previous">
+          <span aria-hidden="true">Н</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="preform2.php">C</a></li>
+      <li class="page-item"><a class="page-link" href="preform3.php">1</a></li>
+      <li class="page-item"><a class="page-link" href="preform4.php">2</a></li>
+      <li class="page-item"><a class="page-link" href="preform5.php">3</a></li>
+      <li class="page-item"><a class="page-link" href="preform6.php">4</a></li>
+      <li class="page-item"><a class="page-link" href="preform7.php">5</a></li>
+      <li class="page-item"><a class="page-link" href="preform8.php">6</a></li>
+      <li class="page-item"><a class="page-link" href="preform9.php">7</a></li>
+      <li class="page-item"><a class="page-link" href="preform10.php">П1</a></li>
+      <li class="page-item"><a class="page-link" href="preform11.php">П2</a></li>
+      <li class="page-item"><a class="page-link" href="preform12.php">П3</a></li>
+      <li class="page-item"><a class="page-link" href="preform13.php">П4</a></li>
+      <li class="page-item">
+        <a class="page-link" href="preform14.php" aria-label="Next">
+          <span aria-hidden="true">П5</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <div class="row">
+    <div class="col-12">
+      <i>
+      <div class="row">
+        <div class="col-12">
+          <table>
+            <caption><b>Приложение 5</b> - Форма № 5 технологии &laquo;Народное Вече&raquo;</caption>
+            <tr>
+              <td><img src="resource/img/4.jpg"  width="45%"  alt=""></th>
+              <td>
+                <h2><b>Президенту Российской Федерации, <br>Владимиру Владимировичу Путину.</b></h2>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">Как &laquo;гаранту Конституции Российской Федерации, прав и свобод человека и гражданина&raquo; (ст.80, п.2 действующей Конституции РФ), а также главе государства, который &laquo;обеспечивает согласованное функционирование и взаимодействие органов государственной власти&raquo; (ст.80, п.2 действующей Конституции РФ), на основании и в порядке статьи 33 Конституции РФ направляю для сведения непосредственное волеизъявление части народа России в количестве ... человек, осуществленное на основании ст.3, п.2 Конституции РФ в отношении инициативы Правительства РФ о повышении пенсионного возраста.
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2"><b>ФОРМУЛА ВОЛЕИЗЪЯВЛЕНИЯ – На основании своего конституционного права непосредственно участвовать в управлении делами государства (ст.32, п.1 действующей Конституции РФ) я, Гражданин Российской Федерации, находясь в здравом уме и светлой памяти, добровольно и осознанно присоединяю свой голос к непосредственному волеизъявлению части Народа РФ по формуле: &laquo;Запрещаю органам государственной власти РФ изменять сроки наступления (пенсионный возраст) гарантированного ст.39, п.1 действующей Конституции РФ права каждого на социальное обеспечение (пенсию)  по возрасту относительно их состояния на 01 января 2015 года.&raquo;</b> 
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <p>Поставленную на голосование &laquo;формулу волеизъявления&raquo; поддержало ... (цифрами и прописью)  граждан, а ... ответили на неё отрицательно.</p>
+                <p>Наличие у голосовавших граждан активного избирательного права в соответ-ствии со статьями 32, п.3 и ст.60 Конституции РФ проверены.</p>
+                <p>Секретарь Исполкома первого уровня <br>неформального Союза избирателей России <br>по обобщению непосредственного волеизъявления граждан России <br>до Воли многонационального народа РФ <br>... (подпись и дата) ... (фамилия, имя, отчество - полностью)</p>
+                <p>Председатель Исполкома первого уровня <br>неформального Союза избирателей России <br>по обобщению непосредственного волеизъявления граждан России <br>до Воли многонационального народа РФ <br>... (подпись и дата) ... (фамилия, имя, отчество - полностью)</p>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      </i>
+    </div>
+  </div>  
+</div>
+
+<?
+require_once("components/footer.php");
+?>
