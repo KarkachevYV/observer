ac8675cfa79935b67e7446bb9ad7f35fc7e12ba4
Yriu2902VK <pchelovod2902@gmail.com>
2020-06-12 18:18:41 +0300

    дополнено Народное вече

---
+++ preform10.php
@@ -0,0 +1,219 @@
+<?
+$title = "Народное вече";
+require_once("components/header.php");
+?>
+<style>
+  .colortext {
+    font-family: 'Lato', sans-serif;
+    font-size: 1.5vw;
+    color: black;      
+}
+  ol {
+    font-size: 1.0vw;
+  } 
+  table, td, th {
+    font-size: 1.0vw;
+	  border: 1px solid black;
+  }
+</style>
+<div class="container">
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="preform.php" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="preform2.php">2</a></li>
+      <li class="page-item"><a class="page-link" href="preform3.php">3</a></li>
+      <li class="page-item"><a class="page-link" href="preform4.php">4</a></li>
+      <li class="page-item"><a class="page-link" href="preform5.php">5</a></li>
+      <li class="page-item"><a class="page-link" href="preform6.php">6</a></li>
+      <li class="page-item"><a class="page-link" href="preform7.php">7</a></li>
+      <li class="page-item"><a class="page-link" href="preform8.php">8</a></li>
+      <li class="page-item"><a class="page-link" href="preform9.php">9</a></li>
+      <li class="page-item"><a class="page-link" href="preform10.php">10</a></li>
+      <li class="page-item"><a class="page-link" href="preform11.php">11</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <div class="row">
+    <div class="col-12">
+      <i>
+      <div class="row">
+        <div class="col-12">
+          <table>
+            <caption><b>Приложение 1</b> - Форма № 1 технологии &laquo;Народное Вече&raquo;</caption>
+            <tr>
+              <td><img src="resource/img/4.jpg"  width="75%"  alt=""></th>
+              <td>
+                <p>&laquo;1. Носителем суверенитета и единственным источником власти в Российской Федерации является ее многонациональный народ&raquo;.</p>
+                <p>&laquo;2. Народ осуществляет свою власть непосредственно, а также через органы государственной власти и органы местного самоуправления&raquo;.</p>
+                <p class="text-right">(ст.3 действующей Конституции РФ)</p>
+                
+                <p><b>&laquo;1. Граждане Российской Федерации имеют право участвовать в управлении делами государства</b> как <b>непосредственно</b>, так и через своих представителей&raquo;.</p>
+                <p class="text-right">(ст.32 действующей Конституции РФ)</p>
+                
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <h4 class="text-center"><b>ЛИСТ НЕПОСРЕДСТВЕННОГО ВОЛЕИЗЪЯВЛЕНИЯ ЧАСТИ НАРОДА РФ</b></h4>
+                <p>В Правительстве Российской Федерации готовится вопрос о поэтапном повышении пенсионного возраста для граждан РФ под предлогом повышения продолжительности жизни в нашей стране. Авторы этой инициативы не понимают разницы между этими терминами или лукавят…</p>
+                <p>Пенсионный возраст 60 лет для мужчин и 55 для женщин вместе со льготами для ряда категорий граждан с тяжелыми условиями труда были установлены с 1928 года на основе исследований о снижении с возрастом способности организма к интенсивному труду в течение рабочего дня и с тех пор не менялись.</p>
+                <p>Средняя продолжительность жизни по стране является условным средне-арифметическим показателем возраста всех умерших в течение данного года людей,  косвенно зависит от уровня развития медицины и никак не влияет на способность человека к труду.</p>
+                <p><b>На основании своего конституционного права непосредственно участвовать в управлении делами государства (ст.32, п.1 действующей Конституции РФ) я, Гражданин Российской Федерации, находясь в здравом уме и светлой памяти, добровольно и осознанно присоединяю свой голос к непосредственному волеизъявлению части Народа РФ по формуле:</b> <b><span class="colortext">&laquo;Запрещаю органам государственной власти РФ изменять сроки наступления (пенсионный возраст) гарантированного ст.39, п.1 действующей Конституции РФ права каждого на социальное обеспечение (пенсию)  по возрасту относительно их состояния на 01 января 2015 года&raquo;.</span></b></p>
+                <p><b>Настоящим подтверждаю, что мое волеизъявление по приведенной формуле голосования сделано в единственном экземпляре, т. е. исключительно в настоящем подписном листе.</b></p>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <table width="100%">
+                  <tr>
+                    <th width="5%">№ п/п</th>
+                    <th width="30%">Фамилия, имя, отчество <br>(полностью и разборчиво)</th>
+                    <th width="35%">Телефон, почтовый или  электронный адрес <br>(для подтверждения, при необходимости, волеизъявления)</th>
+                    <th width="10%">Волеизъявление <br>(да – нет)</th>
+                    <th width="20%">Подпись и дата</th>
+                  </tr>
+                  <tr>
+                    <td>1.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>2.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>3.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>4.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>5.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>6.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>7.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>8.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>9.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>10.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>11.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>12.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>13.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>14.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>15.</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td rowspan="2" colspan="2">Обобщенный итог волеизъявления</td> 
+                    <td>Ответили &laquo;ДА&raquo; </td><td></td><td>человек</td>
+                  </tr>
+                  <tr>
+                    <td>Ответили &laquo;НЕТ&raquo; </td><td></td><td>человек</td>
+                  </tr>
+                  <tr>
+                    <td colspan="5">Полномочия граждан Российской Федерации, заполнивших настоящий лист непосредственного волеизъявления, в соответствии со статьями 32, п.3 и ст.60 Конституции проверил и удостоверяю член Исполкома первого уровня &laquo;Народного Вече&raquo;.</td>
+                  </tr>
+                  <tr>
+                    <td colspan="2">Ф.И.О. полностью и разборчиво</td>
+                    <td colspan="2">Паспортные данные, телефон для связи</td>
+                    <td>Подпись и дата</td>
+                  </tr>
+                  <tr>
+                    <td colspan="5">Лист непосредственного волеизъявления части Народа РФ зарегистрирован в Исполкоме 1 уровня независимой  общественной структуры  суммирования воли Граждан в волю Народа РФ &laquo;Народное Вече&raquo; за №            </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      </i>
+    </div>
+  </div>  
+</div>
+
+<?
+require_once("components/footer.php");
+?>
